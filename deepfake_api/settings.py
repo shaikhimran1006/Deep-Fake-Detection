6c119d082c15702609fca82606bf4429afc0dab1
Imran Shaikh <imran1006shaikh@gmail.com>
2025-01-28 02:58:23 +0530

    setting update

--- deepfake_api/settings.py
+++ deepfake_api/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'deepfake_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
